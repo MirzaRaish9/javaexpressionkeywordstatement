46f55b8dada2dcd56c19ced8e8c3aa8355855e67
Raish Mirza <f.mirza.raish99@gmail.com>
2022-07-25 02:03:56 +0530

    Add files via upload

---
+++ javaexkeystatcb.java
@@ -0,0 +1,37 @@
+package expressionStatementcblokMethod;
+
+public class javaexkeystatcb {
+	
+	public static void main (String[] args) {
+	
+	int int2 = 5;
+   int int3 =7 ;
+    int result = int2 + int3;
+    System.out.println("this is my result = " + result);
+	//false null true are define in expression 
+	
+	System.out.println(int2);
+	boolean isMobile = true;
+	if (isMobile == true) {
+		 System.out.println("This is not your Mobile");
+	}
+	
+	//int b = 1000;
+	//int result =  (a * b)  +  (a);
+	 //    System.out.println("result = " + result);
+	     
+	     
+    int score = 20;
+    if (score > 80) {
+    	System.out.println("You Got HighScore");
+    }
+    else {
+    	System.out.println("nice try ");
+    } 
+    
+   
+    }
+	
+}	
+
+
