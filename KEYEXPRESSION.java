72e44466ad91319390ce82bb1b3786c603d503c5
Raish Mirza <f.mirza.raish99@gmail.com>
2022-07-25 02:03:10 +0530

    Add files via upload

---
+++ KEYEXPRESSION.java
@@ -0,0 +1,48 @@
+
+public class KEYEXPRESSION {
+	
+	public static void main (String [] args) {
+		//int int = 5;
+		// we cant use name as a data type;
+		int int2 = 5; // entire line is statement also int2++ and int2--
+		System.out.println(int2);
+		// int             int2        =2 is called whitespaces 
+		
+		// a Mile is Eqaul to 1.609344 KiloMeter
+		
+		double KilloMeter =  1.609344;
+		System.out.println("killoMeter = " + KilloMeter);
+		//int double is a keyword there is 53 types reserved in java of keyword
+		
+		// int2 = 5
+		// killometer = 1.609344 is an expression
+		// also ( between ) is an expression
+		
+	/*	int CricketHighscore = 99; // CricketHighscore =100 IS EXPRESSION
+		
+		if (CricketHighscore >= 99)// CricketHighscore == 100 IS ALSO 
+		{
+			System.out.println("THIS IS THE HIGHSCORE OF ONEDAY MATCH");
+		} 
+		
+		else 
+		{
+			System.out.println("THIS IS NOT HIGHSCORE VALUE");
+		 }
+	}*/
+int CricketHighscore = 99; // CricketHighscore =100 IS EXPRESSION
+		
+if (CricketHighscore >= 99)// CricketHighscore == 100 IS ALSO 
+{
+System.out.println("THIS IS THE HIGHSCORE OF ONEDAY MATCH");
+} 
+		
+else // this is not more readable or not identation of code
+{
+System.out.println("THIS IS NOT HIGHSCORE VALUE");
+}		
+
+
+
+}
+}
